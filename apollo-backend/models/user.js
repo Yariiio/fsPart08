dd997fbbc41bbd8804604a1f7b78a9baf1b8e9f0
Jari Sedergren <sedergren.antero@gmail.com>
2023-07-26 21:03:00 +0300

    added authorization and mad changes to validation, section C completed

--- apollo-backend/models/user.js
+++ apollo-backend/models/user.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
 
-const userSchema = new mongoose.Model({
+const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
         minLength: 3,
+        unique: true,
     },
 })
 
-module.exports('User', userSchema)
+module.exports = mongoose.model('User', userSchema)
--- apollo-backend/resolvers.js
+++ apollo-backend/resolvers.js
@@ -1,7 +1,9 @@
 const { v1: uuid } = require('uuid')
 const Book = require('./models/book')
 const Author = require('./models/author')
+const User = require('./models/user')
 const { GraphQLError } = require('graphql')
+const jwt = require('jsonwebtoken')
 
 const resolvers = {
     Query: {
@@ -14,6 +16,9 @@ const resolvers = {
             return Book.find({})
         },
         allAuthors: async () => Author.find({}),
+        me: (root, args, context) => {
+            return context.currentUser
+        },
     },
 
     Author: {
@@ -27,58 +32,109 @@ const resolvers = {
     },
 
     Mutation: {
-        addBook: async (root, args) => {
-            const response = await Author.findOne({ name: args.author })
-            if (!response) {
+        addBook: async (root, args, context) => {
+            const currentUser = context.currentUser
+            if (currentUser) {
+                const existingAuthor = await Author.findOne({
+                    name: args.author,
+                })
                 if (args.author.length < 4) {
-                    throw new GraphQLError(
-                        'Name must be atleast 4 characters long',
-                        {
+                    throw new GraphQLError('Authors name too short')
+                }
+                if (args.title.length < 5) {
+                    throw new GraphQLError('Books title too short')
+                }
+                if (!existingAuthor) {
+                    const newAuthor = new Author({ name: args.author })
+                    try {
+                        await newAuthor.save()
+                        const book = new Book({ ...args, author: newAuthor })
+                        return book.save()
+                    } catch (error) {
+                        throw new GraphQLError(error.message, {
                             extensions: {
-                                code: 'BAD_USER_INPUT',
-                                invalidArgs: args.author,
+                                invalidArgs: args,
                             },
-                        }
-                    )
-                } else if (args.title.length < 5) {
-                    throw new GraphQLError(
-                        'Title must be atleast 5 characters long',
-                        {
-                            extensions: {
-                                code: 'BAD_USER_INPUT',
-                                invalidArgs: args.title,
-                            },
-                        }
-                    )
-                } else if (await Book.findOne({ title: args.title })) {
-                    throw new GraphQLError('Title must be unique', {
-                        extensions: {
-                            code: 'BAD_USER_INPUT',
-                            invalidArgs: args.title,
-                        },
-                    })
+                        })
+                    }
                 } else {
-                    const newAuthor = new Author({
-                        name: args.author,
-                    })
-
-                    await newAuthor.save()
-
-                    const book = new Book({ ...args, author: newAuthor })
-                    return book.save()
+                    const book = new Book({ ...args, author: existingAuthor })
+                    try {
+                        return book.save()
+                    } catch (error) {
+                        throw new GraphQLError(error.message, {
+                            extensions: {
+                                invalidArgs: args,
+                            },
+                        })
+                    }
                 }
             } else {
-                const book = new Book({ ...args, author: response })
-                return book.save()
+                throw new GraphQLError('not authenticated', {
+                    extensions: {
+                        code: 'BAD_USER_INPUT',
+                    },
+                })
             }
         },
 
-        editAuthor: async (root, args) => {
-            return Author.findOneAndUpdate(
-                { name: args.author },
-                { born: args.setBornTo },
-                { new: true }
-            )
+        editAuthor: async (root, args, context) => {
+            const currentUser = context.currentUser
+            if (currentUser) {
+                try {
+                    const author = await Author.findOne({ name: args.author })
+                    author.born = args.setBornTo
+                    return author.save()
+                } catch (error) {
+                    throw new GraphQLError('updating user failed', {
+                        extensions: {
+                            code: 'BAD_USER_INPUT',
+                            invalidArgs: args.name,
+                            error,
+                        },
+                    })
+                }
+            } else {
+                throw new GraphQLError('not authenticated', {
+                    extensions: {
+                        code: 'BAD_USER_INPUT',
+                    },
+                })
+            }
+        },
+
+        createUser: async (root, args) => {
+            const user = new User({
+                username: args.username,
+                favoriteGenre: args.favoriteGenre,
+            })
+
+            return user.save().catch((error) => {
+                throw new GraphQLError('creating the user failed', {
+                    extensions: {
+                        code: 'BAD_USER_INPUT',
+                        invalidArgs: args.name,
+                        error,
+                    },
+                })
+            })
+        },
+        login: async (root, args) => {
+            const user = await User.findOne({ username: args.username })
+            if (!user || args.password !== 'mysecret') {
+                throw new GraphQLError('wrong credentials', {
+                    extensions: {
+                        code: 'BAD_USER_INPUT',
+                    },
+                })
+            }
+
+            const userForToken = {
+                username: user.username,
+                id: user._id,
+            }
+
+            return { value: jwt.sign(userForToken, process.env.SECRET) }
         },
     },
 }
